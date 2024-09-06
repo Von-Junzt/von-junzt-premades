460904d060366e85fed63ef66d85411c2c3f0648
aushilfsalien <nils.prior@outlook.de>
2024-09-06 22:11:36 +0200

    restructuring the folders

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // Hooks for the rechargeItem function:
-Hooks.on("dnd5e.restComplete", async () => {
-    await rechargeItem();
+Hooks.on("dnd5e.restCompleted", async () => {
+
+    game.critsRevisited.helperFunctions.createChatMessage(game.users.activeGM, "Wyrmbane has been recharged.");
 });
