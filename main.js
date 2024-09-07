ecc60aa3b02dc532517d7c45604b013d8bc7d9ab
aushilfsalien <nils.prior@outlook.de>
2024-09-07 19:44:08 +0200

    hooked rechargeWyrmbane on restCompleted

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-// Hooks for the rechargeItem function:
-Hooks.on("dnd5e.restCompleted", async () => {
+async function rechargeWyrmbane() {
+    // Get all player actors
+    let playerActors = game.actors.filter(actor => actor.hasPlayerOwner);
+    let playerItems = [];
+    // check if Wyrmbane is in their inventory
+    playerActors.forEach(actor => {
+        let wyrmbane = actor.items.filter(item => item.name === "Wyrmbane");
+        if (wyrmbane.length > 0) {
+            playerItems.push(...wyrmbane);
+        }
+    });
 
-    game.critsRevisited.helperFunctions.createChatMessage(game.users.activeGM, "Wyrmbane has been recharged.");
-});
+    // If Wyrmbane is found and has been used, recharge it and create a chat message
+    if (playerItems.length > 0) {
+        for (let wyrmbane of playerItems) {
+            if(!wyrmbane.flags.homebrew.effectAvailable) {
+                await wyrmbane.update({"flags.homebrew.effectAvailable": true});
+                await ChatMessage.create({
+                    content: "Wyrmbane has been recharged.",
+                });
+            } else {
+                console.log("Wyrmbane has not been used.");
+            }
+        }
+    }
+}
+
+Hooks.on("dnd5e.restCompleted", rechargeWyrmbane);
