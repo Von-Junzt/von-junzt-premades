e2c454439f64fb3d874ebad8819fd1904d183a0b
Von-Junzt <nils.prior@outlook.de>
2024-11-29 08:14:01 +0100

    Update toggleDuckingEffect.js
    Refactored script structure to reduce workload.

--- macros/effects/toggleDuckingEffect.js
+++ macros/effects/toggleDuckingEffect.js
@@ -1,39 +1,38 @@
-const effectData = {
-    "name": "Ducking",
-    "transfer": false,
-    "changes": [
-        {
-            "key": "system.skills.ste.bonuses.check",
-            "mode": 2,
-            "value": "+2",
-            "priority": 20
-        },
-        {
-            "key": "system.attributes.movement.walk",
-            "mode": 1,
-            "value": "0.5",
-            "priority": 20
-        }
-    ],
-    "flags": {
-        "dae": {
-            "showIcon": true,
-        }
-    },
-    "img": "modules/levelsautocover/icons/ducking.png",
-    "description": "<p>The creature ducks down. While crouching, the creature gains +1 to Stealth checks, but halves its movement speed.</p>"
-}
 
 export async function toggleDuckingEffect(tokenDocument) {
-    console.log("Ducking toggled");
-    const token = canvas.tokens.get(tokenDocument.id);
-
-    // Only proceed if this is the gm
+	// Only proceed if this is the gm
     if (!game.user.isGM) return;
-
+	// get token and effect
+    const token = canvas.tokens.get(tokenDocument.id);
     const existingEffect = token.actor.effects.find(e => e.name === "Ducking");
+	
     if (token.document.flags.levelsautocover.ducking) {
         if (!existingEffect) {
+			const effectData = {
+			"name": "Ducking",
+			"transfer": false,
+			"changes": [
+				{
+					"key": "system.skills.ste.bonuses.check",
+					"mode": 2,
+					"value": "+2",
+					"priority": 20
+				},
+				{
+					"key": "system.attributes.movement.walk",
+					"mode": 1,
+					"value": "0.5",
+					"priority": 20
+				}
+			],
+			"flags": {
+				"dae": {
+					"showIcon": true,
+				}
+			},
+			"img": "modules/levelsautocover/icons/ducking.png",
+			"description": "<p>The creature ducks down. While crouching, the creature gains +1 to Stealth checks, but halves its movement speed.</p>"
+			}
             await MidiQOL.socket().executeAsGM("createEffects", {actorUuid: token.actor.uuid, effects: [effectData]});
         }
     } else {
