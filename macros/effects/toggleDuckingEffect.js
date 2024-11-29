8fab3b17faebf813056a356b9f4f2539dee436ea
Von-Junzt <nils.prior@outlook.de>
2024-11-29 08:35:15 +0100

    Update toggleDuckingEffect.js
    added comments

--- macros/effects/toggleDuckingEffect.js
+++ macros/effects/toggleDuckingEffect.js
@@ -22,23 +22,27 @@ const DUCKING_EFFECT = {
         }
     },
     img: "modules/levelsautocover/icons/ducking.png",
-    description: "<p>The creature ducks down. While crouching, the creature gains +1 to Stealth checks, but halves its movement speed.</p>"
+    description: "<p>The creature ducks down. While crouching, the creature gains +2 to Stealth checks, but halves its movement speed.</p>"
 };
 
 export async function toggleDuckingEffect(tokenDocument) {
+    // effects are applied once for every client if we don't return on all non-gm machines
     if (!game.user.isGM) return;
     
+    // get token and see if effect is allready applied
     const token = canvas.tokens.get(tokenDocument.id);
     const existingEffect = token.actor.effects.find(e => e.name === "Ducking");
     
+    // check if token is ducking, if yes, add effect if not existing already
     if (token.document.flags.levelsautocover.ducking) {
-        if (!existingEffect) {
+        if (!existingEffect) { 
             await MidiQOL.socket().executeAsGM("createEffects", {
                 actorUuid: token.actor.uuid, 
                 effects: [DUCKING_EFFECT]
             });
         }
     } else {
+        // if token is not ducking, remove effect
         await MidiQOL.socket().executeAsGM("removeEffects", {
             actorUuid: token.actor.uuid, 
             effects: [existingEffect.id]
