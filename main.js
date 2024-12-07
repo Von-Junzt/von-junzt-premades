79450a86e900a30b9a5dc057a626012daa3f42fb
aushilfsalien <nils.prior@outlook.de>
2024-12-07 19:34:43 +0100

    added hook for dr effect

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import {toggleDuckingEffect} from "./macros/effects/toggleDuckingEffect.js";
-import {rechargeWyrmbane} from "./macros/items/rechargeWyrmbane.js";
+import {updateArmorDR} from "./macros/effects/armorDamageReduction.js";
+
 
 // set up a hook to listen for token ducking
 Hooks.on("updateToken", toggleDuckingEffect);
 
-// NOT NEEDED ATM: set up a hook to listen for the rest event
+// set up a hook to listen for item updates
+Hooks.on('updateItem', async (item, changes, options, userId) => {
+    if (game.user.id !== userId) return;
+
+    if (item.type === "equipment" && item.system.equipped && item.system?.isArmor) {
+        await updateArmorDR(item.parent, item);
+    }
+});
+
+// NOT NEEDED ATM: import wyrmbane recharge script and set up a hook to listen for the rest event
+// import {rechargeWyrmbane} from "./macros/items/rechargeWyrmbane.js";
 // Hooks.on("dnd5e.restCompleted", rechargeWyrmbane);
