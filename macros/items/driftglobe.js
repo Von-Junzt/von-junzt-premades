4f91e6699488ffb7a1c979aa4b0152bc31e5c1fe
aushilfsalien <nils.prior@outlook.de>
2025-01-04 00:05:44 +0100

    added object structure to mimic activities

--- macros/items/driftglobe.js
+++ macros/items/driftglobe.js
@@ -2,22 +2,27 @@
  * This script is used to attach/detach the summoned driftglobe's token to the actor's token.
  */
 
-// Get the driftglobe's token
-let driftglobeEffect = actor.effects.find(e => e.name === "Summon: Driftglobe");
-if (!driftglobeEffect) {
-    console.log("Driftglobe effect not found");
-    return;
-};
-let driftglobeUuid = driftglobeEffect.flags?.dnd5e?.dependents[0]?.uuid;
-let driftglobeToken = await fromUuid(driftglobeUuid);
+const driftglobe = {
+    async toggleAttachment(workflow){
+        // Get the driftglobe's token
+        let driftglobeEffect = actor.effects.find(e => e.name === "Summon: Driftglobe");
+        if (!driftglobeEffect) {
+            console.log("Driftglobe effect not found");
+            return;
+        };
+        let driftglobeUuid = driftglobeEffect.flags?.dnd5e?.dependents[0]?.uuid;
+        let driftglobeToken = await fromUuid(driftglobeUuid);
 
-// check if the driftglobe's token is already attached to the actor's token, if not, attach it
-console.log(driftglobeToken);
-let driftGLobeAttachedFlag = driftglobeToken.getFlag('token-attacher', 'parent');
-if (!driftGLobeAttachedFlag) {
-    ui.notifications.info("Attaching Driftglobe to player.");
-    await tokenAttacher.attachElementToToken(driftglobeToken, token, true);
-} else {
-    ui.notifications.warn("Removing Driftglobe token attachment.");
-    await tokenAttacher.detachElementFromToken(driftglobeToken.object, token, true);
+        // check if the driftglobe's token is already attached to the actor's token, if not, attach it
+        console.log(driftglobeToken);
+        let driftGLobeAttachedFlag = driftglobeToken.getFlag('token-attacher', 'parent');
+        if (!driftGLobeAttachedFlag) {
+            ui.notifications.info("Attaching Driftglobe to player.");
+            await tokenAttacher.attachElementToToken(driftglobeToken, token, true);
+        } else {
+            ui.notifications.warn("Removing Driftglobe token attachment.");
+            await tokenAttacher.detachElementFromToken(driftglobeToken.object, token, true);
+        }
+    }
 }
+
