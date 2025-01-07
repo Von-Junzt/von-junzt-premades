c97632e506f61230f71ad114a6020a5a23fa1915
aushilfsalien <nils.prior@outlook.de>
2025-01-07 21:17:26 +0100

    changed flavourtext

--- macros/items/skillSpecialisation.js
+++ macros/items/skillSpecialisation.js
@@ -2,6 +2,12 @@
  * This script makes a skill specialisation item that rolls the specialisation roll and displays the result in a chat message.
  */
 
+// change this to the base skill you want to use
+let skillAbbreviation = 'prc';
+
+// change this to the bonus you want to use
+let skillSpecialisationBonus = 2;
+
 const message = await fromUuid(workflow.itemCardUuid);
 await message.delete()
 
@@ -34,14 +40,30 @@ const ABILITY_NAMES = {
     "cha": "Charisma"
 };
 
-// change this to the base skill you want to use
-let skillAbbreviation = 'prc';
-
-// change this to the bonus you want to use
-let skillSpecialisationBonus = 2;
-
+// get the actor's skill total bonus
 let skillBonusTotal = actor.system.skills[skillAbbreviation].total;
-let flavorText = `<strong>${workflow.activity.name}</strong><br>(${SKILL_NAMES[skillAbbreviation]} bonus +${skillBonusTotal}, Specialisation bonus +${skillSpecialisationBonus})`;
+
+// create chat message
+flavorText = `<div>${workflow.activity.name} Skill Check</div>`;
+
+/*
+let flavorText = `
+<div class="dnd5e2 chat-card">
+    <div class="description">
+        <div style="display: flex; align-items: center; gap: 5px;">
+            <header class="summary">
+                <img src="${workflow.item.img}" class="gold-icon" style="width: 32px; height: 32px;"/>
+                <div class="name-stacked">
+                    <span class="title" style="font-size: 14px; font-style: normal">${workflow.activity.name}</span>
+                    <span class="subtitle" style="font-size: 10px;font-style: normal">(${SKILL_NAMES[skillAbbreviation]} +${skillBonusTotal}, Specialisation +${skillSpecialisationBonus})</span>
+                </div>
+            </header>
+        </div>
+    </div>
+</div>`;
+*/
+
+// create roll data and roll
 let rollData = `1d20 + ${skillBonusTotal} + 2`;
 console.log(rollData);
 const roll = await new Roll(rollData).evaluate();
